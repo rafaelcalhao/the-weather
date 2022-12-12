0c0463864444d3fe2aae3815d36af87efd170a92
Rafael Calhao <rafael@nimbuslab.com.br>
2022-12-12 19:55:10 -0300

    :construction: Adjustment in the tailwind configuration

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,6 +2,19 @@
 module.exports = {
 	content: ['./index.html', './src/**/*.jsx'],
 	theme: {
+		fontFamily: {
+			sans: ['Nunito', 'sans-serif'],
+		},
+		screens: {
+			'2xl': '1440px',
+		},
+		container: {
+			center: true,
+			padding: {
+				DEFAULT: '1rem',
+				sm: '2rem',
+			},
+		},
 		extend: {},
 	},
 	plugins: [],
